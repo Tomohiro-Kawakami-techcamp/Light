ec07aaeafe125580a4cab62ef0632172ef3dbf6f
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-06 15:02:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/light/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.light;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View view = findViewById(R.id.activity_main);
+        view.setBackgroundColor(Color.WHITE);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_light, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        View view = findViewById(R.id.activity_main);
+        switch (item.getItemId()){
+            case R.id.red:
+                view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.green:
+                view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.blue:
+                view.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.white:
+                view.setBackgroundColor(Color.WHITE);
+                return true;
+        }
+        return false;
+    }
+}
